b28afe255e1f0c05a63c39b3a390f4a53a5a5917
dragon-zzuni <165992919+dragon-zzuni@users.noreply.github.com>
2024-05-24 09:42:25 +0900

    Update page4-1.py

--- pages/page4-1.py
+++ pages/page4-1.py
@@ -2,7 +2,6 @@ import streamlit as st
 import speech_recognition as sr
 import pyttsx3
 import google.generativeai as genai 
-from tkinter.tix import COLUMN
 from pyparsing import empty
 
 from pages.page4 import user1
